042c5a8028ace035f85a359c52584d2f5e94f600
bala-amypo <bala@amypo.in>
2025-12-18 09:12:34 +0000

    User commit

--- src/main/java/com/example/demo/service/Impl/ValidationServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/ValidationServiceImpl.java
@@ -1,2 +1,12 @@
 package com.example.demo.service.Impl
-import com.example.demo
+import com.example.demo.service.validationservice;
+import org.springframework.sterotype.Service;
+import com.example.demo.repository.V
+
+
+
+@Service
+public class ValidationServiceImpl implements validationservice{
+    @Autowired ValidationRepository student;
+    @Override
+}
