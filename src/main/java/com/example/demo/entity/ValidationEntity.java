aba5fcdcc976339f54b0fe3ef07093cbcc1e0b02
bala-amypo <bala@amypo.in>
2025-12-19 03:52:04 +0000

    User commit

--- src/main/java/com/example/demo/entity/ValidationEntity.java
+++ src/main/java/com/example/demo/entity/ValidationEntity.java
@@ -9,6 +9,7 @@ import jakarta.validation.constraints.Size;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Max;
+
 @Entity
 public class ValidationEntity{
     @Id
