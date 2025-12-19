57fec3a038505c664b27c39f62c485ffd9a878f6
bala-amypo <bala@amypo.in>
2025-12-19 06:49:05 +0000

    User commit

--- src/main/java/com/example/demo/entity/ValidationEntity.java
+++ src/main/java/com/example/demo/entity/ValidationEntity.java
@@ -1,16 +1,24 @@
 package com.example.demo.entity;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;  
+import jakarta.persistence.GenerationType;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Max;
-
+import lombok.AllArgumentConstructor;
+import lombok.NoArgumentConstructor;
+import lombok.Setter;
+import lombok.Getter;
+import lombok.Data;
 @Entity
+@Data
+@Getter
+@Setter
+@AllArgumentConstructor
+@NoArgumentConstructor
 public class ValidationEntity{
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
@@ -26,44 +34,5 @@ public class ValidationEntity{
     @Max(30)@Positive(message="Age must be a Positive number")
     private Integer age;
 
-    public ValidationEntity(Long id,@NotNull@Size(min=2,max=10,message="must be 2 to 10 characters")String username,@Email
-    String email,
-    @Size(min=2,max=6)
-    @NotNull(message="Password is mandatory")
-    String password,
-    @Max(30)
-    @Positive(message="Age must be a Positive number")
-    Integer age){
-        this.id=id;
-        this.username=username;
-        this.age=age;
-        this.email=email;
-        this.password=password; 
-    }
-    public void setUsername(String username){
-        this.username=username;
-    }
-    public void setEmail(String email){
-        this.email=email;
-    }
-    public void setPassword(String password){
-        this.password=password;
-    }
-    public void setId(Long id){
-        this.id=id;
-    }
-    
-    public String getUsername(){
-        return username;
-    }
-    public String getEmail(){
-        return email;
-    }
-    public String getPassword(){
-        return password;
-    }
-    public Long getId(){
-        return id;
-    } 
-    public ValidationEntity(){}
+
 }
