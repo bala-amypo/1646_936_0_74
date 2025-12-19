e0a045409db7542e031e05b21e05c83fa09d02c8
bala-amypo <bala@amypo.in>
2025-12-19 05:37:04 +0000

    User commit

--- src/main/java/com/example/demo/exception/GlobalExpection.java
+++ src/main/java/com/example/demo/exception/GlobalExpection.java
@@ -1,2 +1,6 @@
 package com.example.demo.exception;
-import org.springframework.web.bind
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@@restController
